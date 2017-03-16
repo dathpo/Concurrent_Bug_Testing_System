df2f926e11e4247c06f24ef44dc6f12edfa1935a
Jordan Dunnachie CSM2014 <jordan.dunnachie.2014@uni.strath.ac.uk>
2017-03-16 11:20:57 +0000

    add context

--- src/testgenerator/TestCase.java
+++ src/testgenerator/TestCase.java
@@ -7,12 +7,16 @@ public class TestCase {
     List<Integer> inputs;
     List<Integer> expectedOutputs;
     List<String> locks;
+    List<String> inputContext;
+    List<String> outputContext;
     
-    public TestCase (int id, List<Integer> inputs, List<Integer> expectedOutputs, List<String> locks) {
+    public TestCase (int id, List<Integer> inputs, List<Integer> expectedOutputs, List<String> locks, List<String> inputContext, List<String> outputContext) {
         this.id = id;
         this.inputs = inputs;
         this.expectedOutputs = expectedOutputs;
         this.locks = locks;
+        this.inputContext = inputContext;
+        this.outputContext = outputContext;
     }
     
     public int getID() {
@@ -29,5 +33,13 @@ public class TestCase {
     
     public List<String> getLocks() {
         return locks;
+    }
+    
+    public List<String> getInputContext() {
+        return inputContext;
+    }
+    
+    public List<String> getOutputContext() {
+        return outputContext;
     }
 }
